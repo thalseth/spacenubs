ffc1a7bb99f30e77b4f72e57664541b93a958cb4
Torleif Halseth <halseth.torleif@gmail.com>
2013-09-13 15:50:19 +0200

    Init

---
+++ src/entities/obstacle.js
@@ -0,0 +1,40 @@
+Obstacle = BaseEntity.extend({
+    defaults: {
+        'speed' : 2,
+    },
+    initialize: function(){
+        var model = this;
+        var entity = Crafty.e("2D, Canvas, Color, Collision")
+          .color("red")
+          .attr({ x: randomNum(600-20), y: 1, w: 20, h: 20, 
+            dX: Crafty.math.randomInt(-10, 10), 
+            dY: Crafty.math.randomInt(5, 10) })
+          .bind('EnterFrame', function () {
+                //hit floor or roof
+                if (this.y <= 0)
+                this.destroy();
+
+                if (this.x <= 0 || this.x >= 800) {
+                    this.dX *= -1;
+                    //this.destroy();    
+                }
+                if(this.x == 799)
+                {
+                    console.log("Out of window");
+                }
+                this.x += this.dX;
+                this.y += this.dY;
+            })
+            .onHit('ufo', function () {
+                console.log("hit");
+            this.destroy();
+            //this.dX *= -1;
+        })
+        model.set({'entity' : entity });
+    }
+});
+
+function randomNum(num){
+    var random =  Math.floor(Math.random()*num);
+    return random;
+}  
---
+++ src/scenes/main.js
@@ -0,0 +1,23 @@
+Crafty.scene("main", function() {
+
+	var elements = [
+        "src/entities/ufo.js",
+        "src/entities/obstacle.js"
+	];
+	
+	//when everything is loaded, run the main scene
+	require(elements, function() {	   
+		sc['ufo'] = new Ufo();
+		 sc['obstacle'] = new Obstacle();
+		console.log("when everything is loaded, run the main scene");
+		setInterval(myMethod, 500);
+	});
+
+	
+
+	function myMethod( )
+	{
+		sc['obstacle'] = new Obstacle();
+	  console.log("new obstacle");
+	}
+});
